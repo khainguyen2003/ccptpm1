d12834358d1e42cedb47251201a1056c4cf38b06
khainguyen2003 <quyet3582@gmail.com>
2024-03-29 14:36:10 +0700

    change date range component

--- frontend/src/app/admin/components/date-range-filter/date-range-filter.component.ts
+++ frontend/src/app/admin/components/date-range-filter/date-range-filter.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
+import { CalendarModule } from 'primeng/calendar';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-date-range-filter',
   standalone: true,
-  imports: [],
+  imports: [PanelModule, CommonModule, CalendarModule],
   templateUrl: './date-range-filter.component.html',
   styleUrl: './date-range-filter.component.scss'
 })
-export class DateRangeFilterComponent {
+export class DateRangeFilterComponent implements OnInit  {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  @Input() rangeDates: Date[] | undefined;
+  @Input() title!: string;
+  @Input() name: string = '';
+  @Input() minDate: Date | null;
+  @Input() maxDate: Date | null;
+  @Output() filterSelected = new EventEmitter<any[]>();
+  rangeChange: Array<any>;
 
+  
+  
+  filterChanged(event): void {
+    
+  }
 }
